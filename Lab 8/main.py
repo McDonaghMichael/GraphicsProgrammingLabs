49e9cf63d6897bf550074649018b8e7dbc7b9112
McDonaghMichael <G00421944@Atu.ie>
2024-11-18 16:19:11 +0000

    Subplots added

--- Lab 8/main.py
+++ Lab 8/main.py
@@ -5,4 +5,10 @@ from matplotlib import pyplot as plt
 img = cv2.imread("ATU.jpg")
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('./image_grey.png', gray_image)
+cv2.imwrite('./ATU_GRAY.jpg', gray_image)
+
+plt.subplot(1, 1,1),plt.imshow(img, cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(1, 1,1),plt.imshow(gray_image, cmap = 'gray')
+plt.title("Gray Scale"), plt.xticks([]), plt.yticks([])
+plt.show()
