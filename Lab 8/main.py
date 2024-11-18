0a0ed47ae64619d72a72adce252b1140ff5be498
McDonaghMichael <G00421944@Atu.ie>
2024-11-18 16:26:13 +0000

    Code cleaned

--- Lab 8/main.py
+++ Lab 8/main.py
@@ -7,8 +7,11 @@ img = cv2.imread("ATU.jpg")
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('./ATU_GRAY.jpg', gray_image)
 
-plt.subplot(2, 2,1),plt.imshow(img, cmap = 'gray')
+rows = 2
+cols = 2
+plt.subplot(rows, rows,1),plt.imshow(img, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(2, 2,2),plt.imshow(gray_image, cmap = 'gray')
+
+plt.subplot(cols, cols,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title("Gray Scale"), plt.xticks([]), plt.yticks([])
 plt.show()
