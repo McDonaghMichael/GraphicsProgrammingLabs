a9c2faef8fc8c0ab8e0b1ff23868e7e874d81101
McDonaghMichael <G00421944@Atu.ie>
2024-11-18 16:54:17 +0000

    Code looks cleaner

--- Lab 8/main.py
+++ Lab 8/main.py
@@ -8,8 +8,15 @@ gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('./ATU_GRAY.jpg', gray_image)
 
 
-rows = 2
-cols = 2
+blurred_gray = cv2.GaussianBlur(gray_image,(3, 3),3)
+blurred_image_13 = cv2.GaussianBlur(gray_image,(13, 13),3)
+
+sobel_x = cv2.Sobel(blurred_image_13,cv2.CV_64F,1,0,ksize=5)
+sobel_y = cv2.Sobel(blurred_image_13,cv2.CV_64F,0,1,ksize=5)
+
+rows = 3
+cols = 3
+
 plt.subplot(rows, rows,1),plt.imshow(cv2.cvtColor(img, 
 cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
@@ -17,14 +24,17 @@ plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(cols, cols,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title("Gray Scale"), plt.xticks([]), plt.yticks([])
 
-plt.subplot(cols, cols,3),plt.imshow(cv2.GaussianBlur(gray_image,(3, 3),3), cmap = 'gray')
+plt.subplot(cols, cols,3),plt.imshow(blurred_gray, cmap = 'gray')
 plt.title("3x3"), plt.xticks([]), plt.yticks([])
 
-plt.subplot(cols, cols,4),plt.imshow(cv2.GaussianBlur(gray_image,(13, 13),3), cmap = 'gray')
+plt.subplot(cols, cols,4),plt.imshow(blurred_image_13, cmap = 'gray')
 plt.title("13x13"), plt.xticks([]), plt.yticks([])
 
-sobelHorizontal = cv2.Sobel(imgIn,cv2.CV_64F,1,0,ksize=5) # x dir
-sobelVertical = cv2.Sobel(imgIn,cv2.CV_64F,0,1,ksize=5) # y dir
+plt.subplot(cols, cols,5),plt.imshow(sobel_x, cmap = 'gray')
+plt.title("Sobel X"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(cols, cols,6),plt.imshow(sobel_y, cmap = 'gray')
+plt.title("Sobel Y"), plt.xticks([]), plt.yticks([])
 
 
 plt.show()
