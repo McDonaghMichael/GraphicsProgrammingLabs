7b24fa3ab038d328da4b30e49fb7ed96693b8090
McDonaghMichael <G00421944@Atu.ie>
2024-12-02 15:23:42 +0000

    Final commit

--- Lab 8/main.py
+++ Lab 8/main.py
@@ -4,19 +4,26 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread("ATU.jpg")
 
+# Basic Gray image
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('./ATU_GRAY.jpg', gray_image)
 
-
+# Blurred gray Image
 blurred_gray = cv2.GaussianBlur(gray_image,(3, 3),3)
+
+# Another gray blurred image with more intensity
 blurred_image_13 = cv2.GaussianBlur(gray_image,(13, 13),3)
 
+# Sobel on X Axis
 sobel_x = cv2.Sobel(blurred_image_13,cv2.CV_64F,1,0,ksize=5)
+
+# Sobel on Y Axis
 sobel_y = cv2.Sobel(blurred_image_13,cv2.CV_64F,0,1,ksize=5)
+
+# Canny Image
 canny = cv2.Canny(blurred_gray,100,2)
 
-rows = 3
-cols = 3
+rows = 3 # How much rows in our popup
+cols = 3 # How much columns in our popup
 
 plt.subplot(rows, rows,1),plt.imshow(cv2.cvtColor(img, 
 cv2.COLOR_BGR2RGB), cmap = 'gray')
@@ -39,6 +46,5 @@ plt.title("Sobel Y"), plt.xticks([]), plt.yticks([])
 
 plt.subplot(cols, cols,7),plt.imshow(canny, cmap = 'gray')
 plt.title("Canny"), plt.xticks([]), plt.yticks([])
-
 
 plt.show()
