9db05278f1f11d9f48ffcce66438e1810297d21a
McDonaghMichael <G00421944@Atu.ie>
2024-12-09 15:25:49 +0000

    Added dst for harris

--- Lab 9/main.py
+++ Lab 9/main.py
@@ -5,3 +5,11 @@ from matplotlib import pyplot as plt
 img = cv2.imread("ATU.jpg")
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+harris_image = gray_image.copy()
+
+blockSize = 2
+aperture_size = 3
+k = 0.04
+
+dst = cv2.cornerHarris(harris_image, blockSize, aperture_size, k)
