0a1b9b78f2fa9d72635ee52b3d954672de678b0d
McDonaghMichael <G00421944@Atu.ie>
2024-12-09 15:37:03 +0000

    Started SHI tOMASI

--- Lab 9/main.py
+++ Lab 9/main.py
@@ -2,25 +2,54 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread("ATU.jpg")
+img = cv2.imread("ATU1.jpg")
+
+
+rows = 3 # How much rows in our popup
+cols = 3 # How much columns in our popup
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 imgHarris = gray_image.copy()
-
 blockSize = 2
 aperture_size = 3
 k = 0.04
-
 dst = cv2.cornerHarris(gray_image, blockSize, aperture_size, k)
-
 threshold = 0.04; 
-
 B = 0
 G = 0
 R = 0
+
 for i in range(len(dst)):
     for j in range(len(dst[i])):
         if dst[i][j] > (threshold*dst.max()):
             cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
 
+# Original Image
+plt.subplot(cols, cols,1)
+plt.imshow(img, cmap = 'gray')
+plt.title("Original")
+plt.xticks([])
+plt.yticks([])
+
+# Gray Scale Image
+plt.subplot(cols, cols,2)
+plt.imshow(gray_image, cmap = 'gray')
+plt.title("Gray Scale")
+plt.xticks([])
+plt.yticks([])
+
+# Harris Corners
+plt.subplot(cols, cols,3)
+plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
+plt.title("Harris Corners")
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(cols, cols,4)
+plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
+plt.title("Harris Corners")
+plt.xticks([])
+plt.yticks([])
+
+plt.show()
