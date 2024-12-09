4392842fceafbf4416a41e7072f04201ac26e5c9
McDonaghMichael <G00421944@Atu.ie>
2024-12-09 15:28:05 +0000

    Plotted corners

--- Lab 9/main.py
+++ Lab 9/main.py
@@ -6,10 +6,21 @@ img = cv2.imread("ATU.jpg")
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-harris_image = gray_image.copy()
+imgHarris = gray_image.copy()
 
 blockSize = 2
 aperture_size = 3
 k = 0.04
 
-dst = cv2.cornerHarris(harris_image, blockSize, aperture_size, k)
+dst = cv2.cornerHarris(gray_image, blockSize, aperture_size, k)
+
+threshold = 0.04; 
+
+B = 0
+G = 0
+R = 0
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
+
