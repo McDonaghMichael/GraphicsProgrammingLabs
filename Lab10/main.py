290584e35bd0b482d173b40d4c0a95dbca5e783c
McDonaghMichael <G00421944@atu.ie>
2024-12-12 22:03:11 +0000

    We can now loop through all the frames

--- Lab10/main.py
+++ Lab10/main.py
@@ -12,3 +12,32 @@ track_history = defaultdict(lambda: [])
 video_path = "runningvideo.mp4"
 cap = cv2.VideoCapture(video_path)  # Open the video for processing
 
+# Process each frame of the video
+while cap.isOpened():
+
+    # Capture a single frame from the video
+    success, frame = cap.read()
+    if success:
+        # Apply the YOLOv11 model to track objects in the frame
+        results = model.track(frame, persist=True, save=False)
+        boxes = results[0].boxes.xywh.cpu() 
+        track_ids = results[0].boxes.id.int().cpu().tolist() 
+        annotated_frame = results[0].plot() 
+        
+        # Loop through the detected objects and their respective tracks
+        for box, track_id in zip(boxes, track_ids):
+            x, y, w, h = box
+            track = track_history[track_id] 
+            track.append((float(x), float(y)))  
+          
+            
+        cv2.namedWindow('YOLOv11 Tracking', cv2.WINDOW_KEEPRATIO)
+        cv2.imshow("YOLOv11 Tracking", annotated_frame)
+        window = cv2.resizeWindow('YOLOv11 Tracking', 1240, 700)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
